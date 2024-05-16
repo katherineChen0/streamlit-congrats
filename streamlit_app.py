7af749c589a256c1069b97edccbff09f1c6a2763
katherineChen0 <145593633+katherineChen0@users.noreply.github.com>
2024-05-16 15:36:17 -0700

    restart streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,57 +1,52 @@
-import os
 from pathlib import Path
 import json
 import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_extras.let_it_rain import rain
 
-#Directories and file paths
-# Get the absolute path of the current Python file
-current_file_path = os.path.realpath(__file__)
-
-# Get the directory containing the current file
-app_dir = os.path.dirname(current_file_path)
-
-# Set APP_DIR environment variable to the directory containing the current file
-os.environ['APP_DIR'] = app_dir
-THIS_DIR = Path(os.getenv('APP_DIR', __file__)).parent
+# Directories and file paths
+THIS_DIR = Path(__file__).parent
 CSS_FILE = THIS_DIR / "style" / "style.css"
 ASSETS = THIS_DIR / "assets"
 LOTTIE_ANIMATION = ASSETS / "animation_holiday.json"
 
-#function to load and display the Lottie animation
-def load_lot_animation(file_path):
+
+# Function to load and display the Lottie animation
+def load_lottie_animation(file_path):
     with open(file_path, "r") as f:
         return json.load(f)
 
-#function to apply snowfall effect
+
+# Function to apply snowfall effect
 def run_snow_animation():
     rain(emoji="❄️", font_size=20, falling_speed=5, animation_length="infinite")
 
-#function to get the name from query parameters
+
+# Function to get the name from query parameters
 def get_person_name():
     query_params = st.experimental_get_query_params()
     return query_params.get("name", ["Friend"])[0]
 
-#page configuration
-st.set_page_config(page_title="Congrats!", page_icon =" 😁 ")
 
-#run snowfall animation
+# Page configuration
+st.set_page_config(page_title="Congrats", page_icon = " 😁 ")
+
+# Run snowfall animation
 run_snow_animation()
 
-#apply custom CSS
+# Apply custom CSS
 with open(CSS_FILE) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-#display header with personalized name
+# Display header with personalized name
 PERSON_NAME = get_person_name()
-st.header(f"Congrats, {PERSON_NAME}! 😁 ", anchor=False)
+st.header(f"Congrats, {PERSON_NAME}! 🥳", anchor=False)
 
-#display Lottie animation
-lottie_animation = load_lot_animation(LOTTIE_ANIMATION)
+# Display the Lottie animation
+lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
 st_lottie(lottie_animation, key="lottie-holiday", height=300)
 
-#personalized message
+# Personalized holiday message
 st.markdown(
-    f"Dear {PERSON_NAME}, i am so proud of you for getting through another year. "
+    f"Dear {PERSON_NAME}, Congrats on making it through another year! 🌟"
 )
